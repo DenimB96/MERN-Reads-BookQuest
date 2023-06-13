0e2efd0b7e67c11b9e2b73ab446c9f23aa3927df
DenimB96 <rlsjr2@live.com>
2023-06-12 21:23:56 -0400

    Deleted API.js file added in and tweaked quieries and mutations files then altered savebooks and searchbooks functions

---
+++ Develop/Client/src/utils/queries.js
@@ -0,0 +1,20 @@
+// Added query
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
